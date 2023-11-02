c383daef5e78f89355c73665f5b80606ccc6fdab
Francisco Moya <franmoyag@gmail.com>
2023-11-02 18:35:53 -0300

    Implementacion de Visual Login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
+  modeloUsuario: string = '';
+  modeloContrasena: string = '';
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  validarCredenciales(){
+    console.log(this.modeloUsuario);
+    console.log(this.modeloContrasena);
+  }
 }
